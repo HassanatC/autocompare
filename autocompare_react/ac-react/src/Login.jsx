659402e1f3be56cb682c6b2e72f877b7430fa561
rehan-c <rehanchoudhery@gmail.com>
2023-10-08 11:40:09 +0100

    Login and Register components

---
+++ autocompare_react/ac-react/src/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './index.css';
+
+function Login() {
+    return (
+        <div className="form-container">
+            <div className="header">Login</div>
+            <form className="url-form">
+                <label>
+                    <p>Email:</p>
+                    <input type="email" placeholder="Enter your email" required />
+                </label>
+                <label>
+                    <p>Password:</p>
+                    <input type="password" placeholder="Enter your password" required />
+                </label>
+                <button type="submit">Login</button>
+            </form>
+            <p>
+                Don't have an account? <a href="/register" style={{ color: 'var(--primary-color)' }}>Register</a>
+            </p>
+        </div>
+    );
+}
+
+export default Login;
---
+++ autocompare_react/ac-react/src/Register.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './index.css';
+
+function Register() {
+    return (
+        <div className="form-container">
+            <div className="header">Register</div>
+            <form className="url-form">
+                <label>
+                    <p>Name:</p>
+                    <input type="text" placeholder="Enter your name" required />
+                </label>
+                <label>
+                    <p>Email:</p>
+                    <input type="email" placeholder="Enter your email" required />
+                </label>
+                <label>
+                    <p>Password:</p>
+                    <input type="password" placeholder="Enter your password" required />
+                </label>
+                <button type="submit">Register</button>
+            </form>
+            <p>
+                Already have an account? <a href="/login" style={{ color: 'var(--primary-color)' }}>Login</a>
+            </p>
+        </div>
+    );
+}
+
+export default Register;
