1566bc0a01735142f8aa7e2e769eb2e1535c5388
HassanatC <hassanat431@hotmail.co.uk>
2023-10-21 10:16:52 +0100

    ui

--- autocompare_react/ac-react/src/Nav.jsx
+++ autocompare_react/ac-react/src/Nav.jsx
@@ -6,8 +6,6 @@ export default function Nav() {
       <nav className="navbar">
         <ul>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Register</Link></li>
         </ul>
       </nav>
 
